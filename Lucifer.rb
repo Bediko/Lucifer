1cefa1d46944df00ba8e7bf4720c2a907fb9095f
Bediko <bediko@chaosdorf.de>
2012-08-09 16:01:10 +0200

    Reformatting code

--- Lucifer.rb
+++ Lucifer.rb
@@ -4,27 +4,27 @@ require 'mechanize'
 require 'password'
 
 fach= { 
-		"Mathematik 1"																			=> "MAT1",
-		"Digitaltechnik" 																		=> "DIG",
-		"Einführung in die Programmierung" 									=> "EPR",
-		"Rechnerarchitektur" 																=> "RAR",
-		"Mathematik 2"																			=> "MAT2",
-		"Mikroprozessortechnik"															=> "MPT",
-		"Algorithmen und Datenstrukturen"										=> "ALD",
-		"Betriebssysteme"																		=> "BSY",
-		"Objektorientierte Anwendungsentwicklung"						=> "OOA",
-		"Betriebswirtschaft und Marketing"									=> "BWM",
-		"Statistik"																					=> "STA",
-		"Graphische Datenverarbeitung und Bildverarbeitung"	=> "GRA",
-		"Interaktive Systeme"																=> "IAS",
-		"Theoretische Konzepte"															=> "THK",
-		"Datennetze und Datenübertragung"										=> "DNU",
-		"Datenbanksysteme"																	=> "DBS",
-		"Datennetzmanagement"																=> "DNM",
-		"Web Engineering"																		=> "WEB",
-		"Wahlpflichtmodul Vorlesung 1"											=> "WPV1",
-		"Numerik für Informatiker"													=> "NUM",
-	}
+    "Mathematik 1"                                      => "MAT1",
+    "Digitaltechnik"                                    => "DIG",
+    "Einführung in die Programmierung"                  => "EPR",
+    "Rechnerarchitektur"                                => "RAR",
+    "Mathematik 2"                                      => "MAT2",
+    "Mikroprozessortechnik"                             => "MPT",
+    "Algorithmen und Datenstrukturen"                   => "ALD",
+    "Betriebssysteme"                                   => "BSY",
+    "Objektorientierte Anwendungsentwicklung"           => "OOA",
+    "Betriebswirtschaft und Marketing"                  => "BWM",
+    "Statistik"                                         => "STA",
+    "Graphische Datenverarbeitung und Bildverarbeitung" => "GRA",
+    "Interaktive Systeme"                               => "IAS",
+    "Theoretische Konzepte"                             => "THK",
+    "Datennetze und Datenübertragung"                   => "DNU",
+    "Datenbanksysteme"                                  => "DBS",
+    "Datennetzmanagement"                               => "DNM",
+    "Web Engineering"                                   => "WEB",
+    "Wahlpflichtmodul Vorlesung 1"                      => "WPV1",
+    "Numerik für Informatiker"                          => "NUM",
+  }
 https://gist.github.com/3304006
 agent = Mechanize.new
 page = agent.get('https://studinfo.hsnr.de/qisserver/servlet/de.his.servlet.RequestDispatcherServlet?state=user&type=0&application=qispos')
@@ -38,27 +38,27 @@ login_form.asdf = mtnr
 login_form.fdsa = password
 page = agent.submit(login_form, login_form.buttons.first)
 begin
-	page = agent.page.link_with(:text =>'Notenspiegel').click
+  page = agent.page.link_with(:text =>'Notenspiegel').click
 rescue
-	pp 'Mtnr oder Password falsch'
-	exit
+  pp 'Mtnr oder Password falsch'
+  exit
 end
 
 notenspiegel=0.0
 anzahl=0.0
 
 page.search('tr').each do |row|
-	name,note,credits = row.xpath('./td[2] | ./td[4] | ./td[6]')
-	if name && note		
-		nbsp = Nokogiri::HTML("&nbsp;").text
-		note=note.content.gsub(nbsp,"").gsub(",",".");
-		credits=credits.content.gsub(nbsp,"");
-		name=name.content.gsub(nbsp,"");
-		next if note.empty?
-		print fach[name]+"\t"+note+"\t"+credits+"\n"
-		notenspiegel+=note.to_f*credits.to_f
-		anzahl+=credits.to_f
-	end
+  name,note,credits = row.xpath('./td[2] | ./td[4] | ./td[6]')
+  if name && note   
+    nbsp = Nokogiri::HTML("&nbsp;").text
+    note=note.content.gsub(nbsp,"").gsub(",",".");
+    credits=credits.content.gsub(nbsp,"");
+    name=name.content.gsub(nbsp,"");
+    next if note.empty?
+    print fach[name]+"\t"+note+"\t"+credits+"\n"
+    notenspiegel+=note.to_f*credits.to_f
+    anzahl+=credits.to_f
+  end
 end
 notenspiegel=notenspiegel/anzahl
 puts notenspiegel
